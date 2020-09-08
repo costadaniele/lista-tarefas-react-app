2ec6d9d9cdb0221f2310e5e10feb960e8a9dfe1e
costadaniele <santoscostadaniele@gmail.com>
2020-09-08 01:43:15 -0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Main from './components/Main';
+import './App.css';
+
+// primeira versão 
+
+export default function App() {
+  return <Main />;
+}
+
+
+//segunda versão
+
+/*
+function App(){
+  return <h1>Hello World!</h1>;
+}
+
+export default App;
+*/
